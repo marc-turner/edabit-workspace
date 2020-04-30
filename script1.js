c582c8fcaa715e487c47258b79313dbcac33f045
Marc Turner <marc.t1967@gmail.com>
2020-04-30 09:32:18 -0400

    add more to 043020

--- script1.js
+++ script1.js
@@ -71,18 +71,18 @@ console.log('CONNNECTED');
 
 // totalVolume([1, 1, 1]) ➞ 1
 
-function createPhoneNumber(numbers) {
-    return (
-        '(' +
-        numbers.slice(0, 3).join('') +
-        ') ' +
-        numbers.slice(3, 6).join('') +
-        '-' +
-        numbers.slice(6, 10).join('')
-    );
-}
+// function createPhoneNumber(numbers) {
+//     return (
+//         '(' +
+//         numbers.slice(0, 3).join('') +
+//         ') ' +
+//         numbers.slice(3, 6).join('') +
+//         '-' +
+//         numbers.slice(6, 10).join('')
+//     );
+// }
 
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
 
 // createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) ➞ "(123) 456-7890"
 
