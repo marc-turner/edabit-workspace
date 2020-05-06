caa7c9b6b47af689faa81acc9ded6cd1c80125f4
Marc Turner <marc.t1967@gmail.com>
2020-05-06 09:33:42 -0400

    add 050620

---
+++ script3.js
@@ -0,0 +1,30 @@
+function censor(str) {
+    return str
+        .split(' ')
+        .map((x) => (x.length <= 4 ? x : x.replace(/[\w]/gi, '*')))
+        .join(' ');
+}
+
+console.log(censor('The code is fourty'));
+
+// censor("The code is fourty") ➞ "The code is ******"
+
+// censor("Two plus three is five") ➞ "Two plus ***** is five"
+
+// censor("aaaa aaaaa 1234 12345") ➞ "aaaa ***** 1234 *****"
+
+function getEquivalent(note) {
+    let sharpArr = ['C#', 'D#', 'F#', 'G#', 'A#'];
+    let flatArr = ['Db', 'Eb', 'Gb', 'Ab', 'Bb'];
+    return sharpArr.includes(note)
+        ? flatArr[sharpArr.indexOf(note)]
+        : sharpArr[flatArr.indexOf(note)];
+}
+
+console.log(getEquivalent('Bb'));
+
+// getEquivalent("D#") ➞ "Eb"
+
+// getEquivalent("Gb") ➞ "F#"
+
+// getEquivalent("Bb") ➞"A#"
