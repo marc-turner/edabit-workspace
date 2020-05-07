9b25fd072b1451395be6b65bbf50cd556f0b5d39
Marc Turner <marc.t1967@gmail.com>
2020-05-07 11:22:09 -0400

    add 050720

--- script3.js
+++ script3.js
@@ -1,11 +1,11 @@
-function censor(str) {
-    return str
-        .split(' ')
-        .map((x) => (x.length <= 4 ? x : x.replace(/[\w]/gi, '*')))
-        .join(' ');
-}
+// function censor(str) {
+//     return str
+//         .split(' ')
+//         .map((x) => (x.length <= 4 ? x : x.replace(/[\w]/gi, '*')))
+//         .join(' ');
+// }
 
-console.log(censor('The code is fourty'));
+// console.log(censor('The code is fourty'));
 
 // censor("The code is fourty") ➞ "The code is ******"
 
@@ -13,18 +13,28 @@ console.log(censor('The code is fourty'));
 
 // censor("aaaa aaaaa 1234 12345") ➞ "aaaa ***** 1234 *****"
 
-function getEquivalent(note) {
-    let sharpArr = ['C#', 'D#', 'F#', 'G#', 'A#'];
-    let flatArr = ['Db', 'Eb', 'Gb', 'Ab', 'Bb'];
-    return sharpArr.includes(note)
-        ? flatArr[sharpArr.indexOf(note)]
-        : sharpArr[flatArr.indexOf(note)];
-}
+// function getEquivalent(note) {
+//     let sharpArr = ['C#', 'D#', 'F#', 'G#', 'A#'];
+//     let flatArr = ['Db', 'Eb', 'Gb', 'Ab', 'Bb'];
+//     return sharpArr.includes(note)
+//         ? flatArr[sharpArr.indexOf(note)]
+//         : sharpArr[flatArr.indexOf(note)];
+// }
 
-console.log(getEquivalent('Bb'));
+// console.log(getEquivalent('Bb'));
 
 // getEquivalent("D#") ➞ "Eb"
 
 // getEquivalent("Gb") ➞ "F#"
 
 // getEquivalent("Bb") ➞"A#"
+
+const longBurp = (num) => 'Bu' + 'r'.repeat(num) + 'p';
+
+console.log(longBurp(5));
+
+// longBurp(3) ➞ "Burrrp"
+
+// longBurp(5) ➞ "Burrrrrp"
+
+// longBurp(9) ➞ "Burrrrrrrrrp"
