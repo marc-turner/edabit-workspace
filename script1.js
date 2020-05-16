a6a2cb12bdaae5d4e23a3552075157d2e657b98c
Marc Turner <marc.t1967@gmail.com>
2020-05-16 10:40:05 -0400

    add 051620

--- script1.js
+++ script1.js
@@ -42,13 +42,75 @@
 //     "--o---o-----"
 //   ]) ➞ [3, 2]
 
-const coneVolume = (h, r) =>
-    r === 0 ? 0 : ((Math.PI * Math.pow(r, 2) * h) / 3).toFixed(2);
+// const coneVolume = (h, r) =>
+//     r === 0 ? 0 : ((Math.PI * Math.pow(r, 2) * h) / 3).toFixed(2);
 
-console.log(coneVolume(15, 6));
+// console.log(coneVolume(15, 6));
 
 // coneVolume(3, 2) ➞ 12.57
 
 // coneVolume(15, 6) ➞ 565.49
 
 // coneVolume(18, 0) ➞ 0
+///////////////////////////////////////////////////////
+// const reverseArr = (num) =>
+//     String(num)
+//         .split('')
+//         .reverse()
+//         .map((x) => +x);
+
+// console.log(reverseArr(1485979));
+
+// reverseArr(1485979) ➞ [9, 7, 9, 5, 8, 4, 1]
+
+// reverseArr(623478) ➞ [8, 7, 4, 3, 2, 6]
+
+// reverseArr(12345) ➞ [5, 4, 3, 2, 1]
+
+// const reverseAndNot = (i) => +[...String(i)]
+//         .reverse()
+//         .concat([...String(i)])
+//         .join('');
+
+// console.log(reverseAndNot(123));
+
+// reverseAndNot(123) ➞ 321123
+
+// ReverseAndNot(152) ➞ 251152
+
+// reverseAndNot(123456789) ➞ 987654321123456789
+
+// const inBox = (arr) =>
+//     arr.filter(
+//         (x) => x.includes('*') && (x.indexOf('*') !== 0 || !x.endsWith('*'))
+//     ).length >= 1;
+
+// console.log(inBox(['###', '#*#', '###']));
+
+// inBox([
+//     "###",
+//     "#*#",
+//     "###"
+//   ]) ➞ true
+
+//   inBox([
+//     "####",
+//     "#* #",
+//     "#  #",
+//     "####"
+//   ]) ➞ true
+
+//   inBox([
+//     "*####",
+//     "# #",
+//     "#  #*",
+//     "####"
+//   ]) ➞ false
+
+//   inBox([
+//     "#####",
+//     "#   #",
+//     "#   #",
+//     "#   #",
+//     "#####"
+//   ]) ➞ false
