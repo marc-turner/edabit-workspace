16a253e1e286570865a1ecd1902fcc5b996c19f4
Marc Turner <marc.t1967@gmail.com>
2020-05-17 11:39:34 -0400

    add 051720

--- script2.js
+++ script2.js
@@ -1,126 +1,97 @@
-// const clearFog = (str) =>
-//     !/[fog]/gi.test(str) ? "It's a clear day!" : str.match(/[^fog]/gi).join('');
+// function century(year) {
+//     let suf = '';
+//     if (year > 2000) {
+//         suf = 'st';
+//     } else {
+//         suf = 'th';
+//     }
+//     // return year % 100 === 0 && year < 2001
+//     //     ? `${year / 100}th century`
+//     //     : `${(year / 100).toFixed()}${suf} century`;
 
-// console.log(clearFog('fogFogFogffffooobirdsandthebeesGGGfogFog'));
+//     return String(year / 100).split('.')[1] >= 1
+//         ? `${+String(year / 100).split('.')[0] + 1}${suf} century`
+//         : 'st';
+// }
 
-// clearFog("sky") ➞ "It's a clear day!"
+// console.log(century(1000));
 
-// clearFog("fogfogfffoooofftreesggfoogfog") ➞ "trees"
+// century(1756) ➞ "18th century"
 
-// clearFog("fogFogFogffffooobirdsandthebeesGGGfogFog") ➞ "birdsandthebees"
+// century(1555) ➞ "16th century"
 
-// function arrayOfMultiples(num, length) {
+// century(1000) ➞ "10th century"
+
+// century(1001) ➞ "11th century"
+
+// century(2005) ➞ "21st century"
+
+// const cmsSelector = (arr, str) => arr.filter((x) => x.toLowerCase().includes(str));
+
+// console.log(cmsSelector(['WordPress', 'Joomla', 'Drupal', 'Magento'], ''));
+
+// cmsSelector(['WordPress', 'Joomla', 'Drupal' ], 'w') ➞ ['WordPress']
+
+// cmsSelector(['WordPress', 'Joomla', 'Drupal', 'Magento' ], 'ru') ➞ ['Drupal']
+
+// cmsSelector(['WordPress', 'Joomla', 'Drupal', 'Magento' ], '') ➞ ['Drupal', 'Joomla', 'Magento', 'WordPress']
+
+// const equation = (s) => eval(s);
+
+// console.log(equation('1+1'));
+
+// equation("1+1") ➞ 2
+
+// equation("7*4-2") ➞ 26
+
+// equation("1+1+1+1+1") ➞ 5
+
+// function returnEndOfNumber(num) {
+//     return /[4-9]0/.test(String(num).split('').pop())? `${num}-TH` :
+//     ? ;
+// }
+
+// console.log(returnEndOfNumber(34));
+
+// returnEndOfNumber(553) ➞ "553-RD"
+
+// returnEndOfNumber(34) ➞ "34-TH"
+
+// returnEndOfNumber(1231) ➞ "1231-ST"
+
+// returnEndOfNumber(22) ➞ "22-ND"
+
+// function changeTypes(arr) {
 //     let newArr = [];
-//     for (let i = 1; i <= length; i++) {
-//         newArr.push(num * i);
+//     let newStr = '';
+//     for (let i = 0; i < arr.length; i++) {
+//         if (typeof arr[i] === 'string') {
+//             newArr.push(arr[i]);
+//         }
 //     }
 //     return newArr;
 // }
 
-// console.log(arrayOfMultiples(7, 5));
+// console.log(changeTypes(['a', 12, true]));
 
-// arrayOfMultiples(7, 5) ➞ [7, 14, 21, 28, 35]
+// changeTypes(["a", 12, true]) ➞ ["A!", 13, false]
 
-// arrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+// changeTypes([13, "13", "12", "twelve"]) ➞ [13, "13!", "12!", "Twelve!"]
 
-// arrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+// changeTypes([false, "false", "true"]) ➞ [true, "False!", "True!"]
 
-// function factorGroup(num) {
-//     return Number.isInteger(Math.sqrt(num)) ? 'even' : 'odd';
-// }
+function tpChecker(home) {
+    let amountNeeded = home.people * 57 * 14;
+    let amountHave = home.tp * 500;
+    let tpSupplyLen = Math.floor(amountHave / (amountNeeded / 14));
+    if (amountNeeded > amountHave) {
+        return `Your TP will only last ${tpSupplyLen} days, buy more!`;
+    }
+    return `Your TP will last ${tpSupplyLen} days, no need to panic!`;
+}
 
-// console.log(factorGroup(33));
+console.log(tpChecker({ people: 2, tp: 4 }));
 
-// factorGroup(33) ➞ "even"
+// {people: 4, tp:1} ➞ "Your TP will only last 2 days, buy more!"
 
-// factorGroup(36) ➞ "odd"
-
-// factorGroup(7) ➞ "even"
-
-// const isSafeBridge = str => !str.includes(' ');
-
-// console.log(isSafeBridge('####'));
-
-// isSafeBridge("####") ➞ true
-
-// isSafeBridge("## ####") ➞ false
-
-// isSafeBridge("#") ➞ true
-
-// function changeEnough(change, amountDue) {
-//     let sum = 0;
-//     const coinVals = [0.25, 0.1, 0.05, .01];
-//     for (let i = 0; i < 4; i++) {
-//         sum = sum + change[i] * coinVals[i];
-//     }
-//     return sum > amountDue;
-// }
-
-// console.log(changeEnough([0, 0, 20, 5], 0.75));
-
-// changeEnough([2, 100, 0, 0], 14.11) ➞ false
-
-// changeEnough([0, 0, 20, 5], 0.75) ➞ true
-
-// changeEnough([30, 40, 20, 5], 12.55) ➞ true
-
-// changeEnough([10, 0, 0, 50], 3.85) ➞ false
-
-// changeEnough([1, 0, 5, 219], 19.99) ➞ false
-
-// function isSastry(num) {
-//     return Number.isInteger(
-//         Math.sqrt(+(num.toString() + (num + 1).toString()))
-//     );
-// }
-
-// console.log(isSastry(183));
-
-// isSastry(183) ➞ true
-// // Concatenation of n and its successor = 183184
-// // 183184 is a perfect square (428 ^ 2)
-
-// isSastry(184) ➞ false
-// // Concatenation of n and its successor = 184185
-// // 184185 is not a perfect square
-
-// isSastry(106755) ➞ true
-// // Concatenation of n and its successor = 106755106756
-// // 106755106756 is a perfect square (326734 ^ 2)
-
-// const stripSentence = (str, chars) =>
-//     str.replace(new RegExp(`[^${chars}]`, 'g'), '');
-
-// console.log(
-//     stripSentence('the quick brown fox jumps over the lazy dog', 'aeiou')
-// );
-
-// stripSentence("the quick brown fox jumps over the lazy dog", "aeiou") ➞ "th qck brwn fx jmps vr th lzy dg"
-
-// stripSentence("the hissing snakes sinisterly slither across the rustling leaves", "s") ➞ "the hiing nake initerly lither acro the rutling leave"
-
-// stripSentence("gone, reduced to atoms", "go, muscat nerd") ➞ ""
-
-// function concat(...args) {
-//     return args.flat(1);
-// }
-
-// console.log(concat([1, 2, 3], [4, 5], [6, 7]));
-
-// concat([1, 2, 3], [4, 5], [6, 7]) ➞ [1, 2, 3, 4, 5, 6, 7]
-
-// concat([1], [2], [3], [4], [5], [6], [7]) ➞ [1, 2, 3, 4, 5, 6, 7]
-
-// concat([1, 2], [3, 4]) ➞ [1, 2, 3, 4]
-
-// concat([4, 4, 4, 4, 4]) ➞ [4, 4, 4, 4, 4]
-
-const triangle = (n) => (n === 1 ? 1 : (n * (n + 1)) / 2);
-
-console.log(triangle(215));
-
-// triangle(1) ➞ 1
-
-// triangle(6) ➞ 21
-
-// triangle(215) ➞ 23220
+// {people: 3, tp:20} ➞ "Your TP will last 58 days, no need to panic!"
