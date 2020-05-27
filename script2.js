08ee855961ae213fcdcfe666cdeba06e673e04f6
Marc Turner <marc.t1967@gmail.com>
2020-05-27 12:49:41 -0400

    add 052720

--- script2.js
+++ script2.js
@@ -1,49 +1,11 @@
-// function smashFactor(bs, cs) {
-//     return (bs / cs).toFixed(2);
-// }
+function arrayOperation(x, y, n) {
+    return x;
+}
 
-// console.log(smashFactor(139.4, 93.8));
+console.log();
 
-// smashFactor(139.4, 93.8) ➞ 1.49
+// arrayOperation(1, 10, 3) ➞ [3, 6, 9]
 
-// smashFactor(181.2, 124.5) ➞ 1.46
+// arrayOperation(7, 9, 2) ➞ [8]
 
-// smashFactor(154.7, 104.3) ➞ 1.48
-
-// function eq(evaluate) {
-//     return eval(evaluate);
-// }
-
-// console.log(eq('3+2-4'));
-
-// eq("1+2") ➞ 3
-
-// eq("6/(9-7)") ➞ 3
-
-// eq("3+2-4") ➞ 1
-
-// function stringInt(str) {
-//     return +str;
-// }
-
-// console.log(stringInt('6'));
-
-// stringInt("6") ➞ 6
-
-// stringInt("1000") ➞ 1000
-
-// stringInt("12") ➞ 12
-
-// function doubleLetters(word) {
-//     return /(\w{2})/gi.test(word);
-// }
-
-// console.log(doubleLetters('orange'));
-
-// doubleLetters("loop") ➞ true
-
-// doubleLetters("yummy") ➞ true
-
-// doubleLetters("orange") ➞ false
-
-// doubleLetters("munchkin") ➞ false
+// arrayOperation(15, 20, 7) ➞ []
