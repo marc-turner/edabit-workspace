33019dd6acb5524ec58f6a111aa7ff2110039ee7
Marc Turner <marc.t1967@gmail.com>
2020-05-28 10:30:59 -0400

    add 052820v2

--- script2.js
+++ script2.js
@@ -38,3 +38,42 @@
 // sumTwoSmallestNums([879, 953, 694, -847, 342, 221, -91, -723, 791, -587]) ➞ 563
 
 // sumTwoSmallestNums([3683, 2902, 3951, -475, 1617, -2385]) ➞ 4519
+// /////////////////////////////////////////////////////////
+// function counterpartCharCode(char) {
+//     return char == char.toLowerCase()
+//         ? char.toUpperCase().charCodeAt(0)
+//         : char.toLowerCase().charCodeAt(0);
+// }
+
+// console.log(counterpartCharCode('è'));
+
+// counterpartCharCode("A") ➞ 97
+
+// counterpartCharCode("a") ➞ 65
+// **********************************
+// function alphNum(str) {
+//     const ab = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//     return [...str].map((x) => ab.indexOf(x)).join(' ');
+// }
+
+// console.log(alphNum('XYZ'));
+
+// alphNum("XYZ") ➞ "23 24 25"
+
+// alphNum("ABCDEF") ➞ "0 1 2 3 4 5"
+
+// alphNum("JAVASCRIPT") ➞ "9 0 21 0 18 2 17 8 15 19"
+// ************************************
+// const validatePIN = (pin) =>
+//     pin.length !== 4 && pin.length !== 6 ? false : /^\d+$/.test(pin);
+
+// console.log(validatePIN('1234'));
+
+// validatePIN("1234") ➞ true
+
+// validatePIN("12345") ➞ false
+
+// validatePIN("a234") ➞ false
+
+// validatePIN("") ➞ false
+// ////////////////////////////////////////////////////////
